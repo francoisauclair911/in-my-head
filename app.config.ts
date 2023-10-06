84cf297af8835960f2b71c6e28c307b6b2924a02
francoisauclair911 <francois.auclair.911@gmail.com>
2023-10-06 21:31:28 +0200

    feat: new title

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,6 @@
 export default defineAppConfig({
   alpine: {
-    title: 'Alpine',
+    title: 'In my head',
     description: 'The minimalist blog theme',
     image: {
       src: '/social-card-preview.png',
